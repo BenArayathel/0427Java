e9aadddaadfd6f1edf2721e99594ac7cb720c2bc
Ben Swerdlow <benswerd@umich.edu>
2020-04-29 18:13:25 -0400

    Partial completion of ID Validator. Need to look up details about state ids.

--- Ben_Swerdlow_Code/Homework/src/com/examples/regex/IdValidator.java
+++ Ben_Swerdlow_Code/Homework/src/com/examples/regex/IdValidator.java
@@ -2,11 +2,43 @@ package com.examples.regex;
 
 public class IdValidator {
 	
-	public static boolean driversLicense(String license) {
-		if (license.matches("<REGEX HERE>")) {
+//	public static boolean driversLicense(String license, String state) {
+//		switch (state.toLowerCase()) {
+//			case("michigan"):
+//				String regex = "[A-Z]{1}";
+//				break;
+//			case("florida"):
+//				String regex = "[A-Z]{1}";
+//				break;
+//			case("georgia"):
+//				String regex = "[A-Z]{1}";
+//				break;
+//			case("dc"):
+//				String regex = "[A-Z]{1}";
+//				break;
+//			default:
+//				return false;
+//				// Fix this to error message later
+//		}
+//		if (license.matches(regex)) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+	
+	public static boolean SSN(String ssn) {
+		if (ssn.replace(" ", "").replace("-", "").matches("[0-9]{9}")) {
 			return true;
 		} else {
-			return false;	
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		String[] testSsnList = {"889-27-5849", "123456789", "asdf12354546", "1230189273058912374058917", "1234---    --78453"};
+		for (String ssn : testSsnList) {
+			System.out.println(IdValidator.SSN(ssn));
 		}
 	}
 
