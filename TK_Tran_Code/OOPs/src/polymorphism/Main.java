58874beb5eee4fa753670c86bd626637ecaa22ba
TK Tran <tktrannn@live.com>
2020-04-29 00:31:51 -0400

    Completed polymorphism.

--- TK_Tran_Code/OOPs/src/polymorphism/Main.java
+++ TK_Tran_Code/OOPs/src/polymorphism/Main.java
@@ -2,6 +2,17 @@ package polymorphism;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Animal myAnimal = new Animal();
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
     }
 }
+
+/* 
+    Polymorphism: when inherited methods can have multiple meanings (implementations).
+    Why and When to Use Inheritance and Polymorphism?
+        - Code reusability.
+*/
---
+++ TK_Tran_Code/OOPs/src/polymorphism/Pig.java
@@ -0,0 +1,8 @@
+package polymorphism;
+
+public class Pig extends Animal {
+
+    public void animalSound() {
+        System.out.println("Pigs say wee wee!");
+    }
+}
