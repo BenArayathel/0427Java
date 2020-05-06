80649cb299a7267367db5ab21ba97ff8ebd629f5
Ben Swerdlow <benswerd@umich.edu>
2020-05-05 22:58:00 -0400

    removed unused import

--- Ben_Swerdlow_Code/date_time_api_demo/src/reading_date_and_validation/ValidationBusiness.java
+++ Ben_Swerdlow_Code/date_time_api_demo/src/reading_date_and_validation/ValidationBusiness.java
@@ -4,8 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.management.RuntimeErrorException;
-
 public class ValidationBusiness {
 	
 	public boolean isValidDate(String s) throws RuntimeException {
