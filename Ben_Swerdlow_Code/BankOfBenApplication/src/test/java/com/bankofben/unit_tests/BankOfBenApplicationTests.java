5200fc147369b08ad1a52359c8a39aaef880a905
Ben Swerdlow <benswerd@umich.edu>
2020-05-05 22:58:55 -0400

    Made better outline for unit tests

--- Ben_Swerdlow_Code/BankOfBenApplication/src/test/java/com/bankofben/unit_tests/BankOfBenApplicationTests.java
+++ Ben_Swerdlow_Code/BankOfBenApplication/src/test/java/com/bankofben/unit_tests/BankOfBenApplicationTests.java
@@ -1,5 +1,51 @@
 package com.bankofben.unit_tests;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class BankOfBenApplicationTests {
+	/*
+	 * These are unit tests for the Bank of Ben Application (i.e. the user interface)
+	 */
+	
+	// Will be executed before testing begins
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Getting ready for all the Application tests\n");
+	}
+	
+	// Will be executed after each test
+	@Before
+	public static void before() {
+		System.out.println("\nAttempting the next Application test");
+	}
+	
+	@Test
+	public static void registerUser() {
+		// Test the user registration process
+//		assertArrayEquals(expecteds, actuals);
+	}
+	
+	@Test
+	public static void approve() {
+		// Test the 
+	}
+	
+	// Will be executed after each test
+	@After
+	public void testEquals() {
+		System.out.println("Completed test");
+	}
+	
+	// Will be executed after testing begins
+	@AfterClass
+	public static void after() {
+		
+	}
 
 }
---
+++ Ben_Swerdlow_Code/BankOfBenApplication/src/test/java/com/bankofben/unit_tests/BankTests.java
@@ -0,0 +1,41 @@
+package com.bankofben.unit_tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BankTests {
+	/*
+	 * These are tests for things that the BankOfBen class can do (e.g. register users and turn them into customers)
+	 */
+
+	// Will be executed before testing begins
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Getting ready for all User Tests\n");
+	}
+	
+	// Will be executed after each test
+	@Before
+	public static void before() {
+		System.out.println("\nAttempting the next User Test");
+	}
+	
+	@Test
+	public static void applyForAccount() {
+		
+	}
+	
+	@After
+	public static void after() {
+		
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		
+	}
+	
+}
