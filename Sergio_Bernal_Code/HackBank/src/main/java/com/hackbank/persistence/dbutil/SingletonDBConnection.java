d67a0e3d00a93257fd5bfa801dc287a127966628
serberestra <serberestra@gmail.com>
2020-05-10 23:46:15 -0400

    New information...

--- Sergio_Bernal_Code/HackBank/src/main/java/com/hackbank/persistence/dbutil/SingletonDBConnection.java
+++ Sergio_Bernal_Code/HackBank/src/main/java/com/hackbank/persistence/dbutil/SingletonDBConnection.java
@@ -8,10 +8,6 @@ public class SingletonDBConnection {
 	
 	private static Connection conn;
 	
-//	private static String URL = "jdbc:oracle:thin:@hackbank.c0qga0bhdqei.us-east-2.rds.amazonaws.com:1521:orcl";
-//	private static String USER = "sbadmin";
-//	private static String PASSWORD = "sys.0123";
-	
 	private static String URL = System.getenv("HACKBANK_URL");
 	private static String USER = System.getenv("HACKBANK_USER");
 	private static String PASSWORD = System.getenv("HACKBANK_PWD");
