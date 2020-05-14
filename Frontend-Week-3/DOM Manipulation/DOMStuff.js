1c26567fa5e53f59079e0c7062f64520db99c3df
markwartman1 <markwartman1@gmail.com>
2020-05-14 10:26:33 -0500

    comment DOM stuff

--- Frontend-Week-3/DOM Manipulation/DOMStuff.js
+++ Frontend-Week-3/DOM Manipulation/DOMStuff.js
@@ -33,19 +33,19 @@ InnerHTML: HTML tags will be registered as HTML
 //-------------------------------------------------------------------------
 
 //Let's get elements by class 
-let para2 = document.getElementsByClassName("headers");
-// console.log(para2);
-para2[0].innerHTML = "What's gonna happen?"
+// let para2 = document.getElementsByClassName("headers");
+// // console.log(para2);
+// para2[0].innerHTML = "What's gonna happen?"
 
 //-------------------------------------------------------------------------
 
 // Get elements by tag name 
 
-let para3 = document.getElementsByTagName("p");
+// let para3 = document.getElementsByTagName("p");
 
-for(let i = 0; i<para3.length; i++){
-    para3[i].innerText = "Modifying all the p tags " + i;
-}
+// for(let i = 0; i<para3.length; i++){
+//     para3[i].innerText = "Modifying all the p tags " + i;
+// }
 
 //-------------------------------------------------------------------------
 
@@ -53,30 +53,30 @@ for(let i = 0; i<para3.length; i++){
 
 //prefixes: # for id, . for class, no prefix for a tag
 
-let selection = document.querySelector("#para2"); 
-selection.innerText = "shtuff";
+// let selection = document.querySelector("#para2"); 
+// selection.innerText = "shtuff";
 
-let selection2 = document.querySelector(".para");//return the first element of the class type
-selection2.innerText = "more shtuff";
+// let selection2 = document.querySelector(".para");//return the first element of the class type
+// selection2.innerText = "more shtuff";
 
-let selection3 = document.querySelectorAll(".para");
+// let selection3 = document.querySelectorAll(".para");
 // console.log(selection3);
 
-let oSelection = document.getElementsByClassName("para");
+// let oSelection = document.getElementsByClassName("para");
 // console.log(oSelection);
 
 // console.log(oSelection["para1"]);
 // console.log(selection3["para1"]);
 
-for(let i = 0; i<selection3.length; i++){
-    selection3[i].innerText = "No more shtuff";
-}
+// for(let i = 0; i<selection3.length; i++){
+//     selection3[i].innerText = "No more shtuff";
+// }
 
 
 //-------------------------------------------------------------------------
 
-let selection4 = document.querySelector(".para:nth-child(4)");
-console.log(selection4);
+// let selection4 = document.querySelector(".para:nth-child(4)");
+// console.log(selection4);
 
 /*
 You can also use stuff like:
@@ -88,22 +88,22 @@ You can also use stuff like:
     also, next and previous siblings... etc.
 */
 
-console.log(selection2.parentNode.nodeType);
-console.log(selection2.parentElement);
+// console.log(selection2.parentNode.nodeType);
+// console.log(selection2.parentElement);
 
 //-------------------------------------------------------------------------
 
 //CREATE ELEMENTS
-let newDiv = document.createElement("div");
+// let newDiv = document.createElement("div");
 
 //Step 2: populate our new element with attributes
-newDiv.id = "newestDiv";
-newDiv.setAttribute("title","new div, who dis?");
+// newDiv.id = "newestDiv";
+// newDiv.setAttribute("title","new div, who dis?");
 
 //Step 3: create a text node then append to our new div element.
-let divText = document.createTextNode("new text node riiiiight here!!");
-newDiv.appendChild(divText);
+// let divText = document.createTextNode("new text node riiiiight here!!");
+// newDiv.appendChild(divText);
 
 //Step 4: appending our new div elementt into our doc
-let newSelection = document.querySelector("#para1");
-newSelection.appendChild(newDiv);
+// let newSelection = document.querySelector("#para1");
+// newSelection.appendChild(newDiv);
