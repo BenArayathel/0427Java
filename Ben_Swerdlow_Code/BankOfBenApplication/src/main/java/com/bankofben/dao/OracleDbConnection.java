56e5e34c1c82f204048d5c88071610c61466b06d
Ben Swerdlow <benswerdlow1005@gmail.com>
2020-05-14 01:44:39 -0400

    Changed code to get the path to the project first, then navigate from there to the login info text file so it will work on different computers

--- Ben_Swerdlow_Code/BankOfBenApplication/src/main/java/com/bankofben/dao/OracleDbConnection.java
+++ Ben_Swerdlow_Code/BankOfBenApplication/src/main/java/com/bankofben/dao/OracleDbConnection.java
@@ -1,6 +1,7 @@
 package com.bankofben.dao;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
@@ -8,10 +9,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.bankofben.exceptions.BusinessException;
 
 public class OracleDbConnection {
+	
+//	final static Logger loggy = Logger.getLogger(OracleDbConnection.class);
 	
 	private static Connection connection = null;
 	
@@ -19,16 +23,21 @@ public class OracleDbConnection {
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException, BusinessException {
 		Class.forName("oracle.jdbc.OracleDriver");
+		
+		String projectPath = new File("").getAbsolutePath();
+		
 		if (connection==null) {
 			
 			List<String> login = new ArrayList<>();
 			
-			try(BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\bensw\\revature_training\\BankOfBenDbLogin.txt"))){
+			try(BufferedReader reader = new BufferedReader(new FileReader(projectPath+"\\..\\..\\..\\BankOfBenDbLogin.txt"))){
 				String line = "";
 				while ((line = reader.readLine()) != null) {
 					login.add(line);
 				}
 			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println(e);
 				throw new BusinessException("Could not read login information. Please check your login information and try again.");
 			}
 			connection = DriverManager.getConnection(login.get(0), login.get(1), login.get(2));
@@ -37,7 +46,7 @@ public class OracleDbConnection {
 			
 			List<String> login = new ArrayList<>();
 			
-			try(BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\bensw\\revature_training\\BankOfBenDbLogin.txt"))){
+			try(BufferedReader reader = new BufferedReader(new FileReader(projectPath+"\\..\\..\\..\\BankOfBenDbLogin.txt"))){
 				String line = "";
 				while ((line = reader.readLine()) != null) {
 					login.add(line);
