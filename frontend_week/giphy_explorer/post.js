818e32a840e79fc511e683fc2b90a155f6997681
gunderodd <gunderodd@hotmail.com>
2020-05-15 07:45:05 -0700

    styling, script files stopped working

---
+++ frontend_week/giphy_explorer/post.js
@@ -0,0 +1,15 @@
+// let newName = 'John Smith'
+// let xhttp = new XMLHttpRequest();
+
+// // xhttp.open('POST', 'myservice/username?id=some-unique-id');
+// xhttp.open('POST', 'http://127.0.0.1:5500/api_eater/index.html');
+// xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// xhttp.onload = function() {
+//     if (xhttp.status === 200 && xhttp.responseText !== newName) {
+//         alert('Something went wrong.  Name is now ' + xhttp.responseText);
+//     }
+//     else if (xhttp.status !== 200) {
+//         alert('Request failed.  Returned status of ' + xhttp.status);
+//     }
+// };
+// xhttp.send(encodeURI('name=' + newName));
