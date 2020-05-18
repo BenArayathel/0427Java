9691efd56b1cdf98f4a236b150f8d5113a7c5c91
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-05-18 19:41:21 -0400

    starting servlets demo

---
+++ Maxwell_DePriest_Code/ProjectOne/com.hello.servlets/src/main/java/com/hello/servlets/HelloServelet.java
@@ -0,0 +1,17 @@
+package com.hello.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServelet extends HttpServlet {
+	
+	
+	@Override 
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("Inside doGet request");
+	}
+}
