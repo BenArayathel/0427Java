f79f1d6ed2f6136ff8616e3177aa2424c6fa246c
vinay <vinay.ingalahalli1989@gmail.com>
2020-05-21 03:12:29 +0530

    filter session and cookie demos

---
+++ usf-batch-workspace/session_demo/src/com/controller/SecondServlet.java
@@ -0,0 +1,45 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model.User;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession(false);
+		if(session==null) {
+			response.sendRedirect("/session_demo");
+		}else {
+			User user=(User) session.getAttribute("user");
+			user.setJob(request.getParameter("job"));
+			user.setRole(request.getParameter("role"));
+			user.setOrg(request.getParameter("org"));
+			session.setAttribute("user", user);
+			response.sendRedirect("three.html");
+		}
+	}
+
+}
