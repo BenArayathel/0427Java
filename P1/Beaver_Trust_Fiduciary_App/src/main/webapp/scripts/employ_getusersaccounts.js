b5a07ee7d3d84740f921065e316331c9730345b9
gunderodd <gunderodd@hotmail.com>
2020-05-23 23:31:43 -0700

    employee can approve accounts

--- P1/Beaver_Trust_Fiduciary_App/src/main/webapp/scripts/employ_getusersaccounts.js
+++ P1/Beaver_Trust_Fiduciary_App/src/main/webapp/scripts/employ_getusersaccounts.js
@@ -1,9 +1,9 @@
 let userAccountsList = document.getElementById("userAccountsList");
 let submit5 = document.getElementById("seeAllUserAcounts");
 
-submit5.addEventListener("click", deposit);
+submit5.addEventListener("click", getAccounts);
 
-function deposit() {
+function getAccounts() {
     userAccountsList.style.display = "block";
     let username = document.getElementById("whichUser").value;
 
@@ -18,21 +18,21 @@ function deposit() {
             username: username,
         })
     })
-        .then(response => {
-            console.log(response)
-            return response.json();
-        })
-        .then(json => {
-            // console.log(json)
-            for (let i in json) {
-                let li = document.createElement("LI");
-                let transInfo = document.createTextNode
-                (`Account Name: ${json[i].account_name} 
-                    Account_ID: ${json[i].account_id}    
-                        User ID: ${json[i].user_id}
-                            Balance: ${json[i].balance}`);
-                li.appendChild(transInfo);
-                userAccountsList.appendChild(li);
-            }
-        })
+    .then(response => {
+        console.log(response)
+        return response.json();
+    })
+    .then(json => {
+        // console.log(json)
+        for (let i in json) {
+            let li = document.createElement("LI");
+            let transInfo = document.createTextNode
+            (`Account Name: ${json[i].account_name} 
+                Account_ID: ${json[i].account_id}    
+                    User ID: ${json[i].user_id}
+                        Balance: ${json[i].balance}`);
+            li.appendChild(transInfo);
+            userAccountsList.appendChild(li);
+        }
+    })
 }
