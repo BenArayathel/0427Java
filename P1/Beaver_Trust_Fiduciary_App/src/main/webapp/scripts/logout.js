0a427728a7595428537f5c42b6f6b4a0fd303477
gunderodd <gunderodd@hotmail.com>
2020-06-02 21:30:15 -0700

    started ORM Beans assigment

--- P1/Beaver_Trust_Fiduciary_App/src/main/webapp/scripts/logout.js
+++ P1/Beaver_Trust_Fiduciary_App/src/main/webapp/scripts/logout.js
@@ -6,6 +6,8 @@ logoutButton.addEventListener("click", logout);
 
 // get is default, doesn't need to be stated
 function logout() {
+    // window.history.forward();
+    // function noBack() { window.history.forward(); }
     fetch('http://localhost:9999/Beaver_Trust_Fiduciary_App/logout')
         .then(
             console.log("Session terminated")
