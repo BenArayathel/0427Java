7cf27287958a6d0b9be3768cb0af5eeb0727ee89
Ben Swerdlow <benswerdlow1005@gmail.com>
2020-06-05 11:17:14 -0400

    Setting up POJO and repo

--- Ben_Swerdlow_Code/ProtestOrganizer/src/main/java/com/protestorganizer/dao/ProtestRepo.java
+++ Ben_Swerdlow_Code/ProtestOrganizer/src/main/java/com/protestorganizer/dao/ProtestRepo.java
@@ -1,5 +1,43 @@
 package com.protestorganizer.dao;
 
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.protestorganizer.model.Protest;
+
+@Repository("protestRepo")
+@Transactional
 public class ProtestRepo {
+	
+	private SessionFactory sesFact;
+	
+	@Autowired
+	public ProtestRepo(SessionFactory sesFact) {
+		this.sesFact = sesFact;
+	}
+	
+	public void insert(Protest p) {
+		sesFact.getCurrentSession().save(p);
+	}
+	
+	public List<Protest> selectAll() {
+		return sesFact.getCurrentSession().createQuery("from Protest", Protest.class).list();
+	}
+	
+	public Protest selectById(int id) {
+		return sesFact.getCurrentSession().createQuery("from Protest where id="+id, Protest.class).list().get(0);
+	}
+	
+	public void update(Protest p) {
+		sesFact.getCurrentSession().update(p);
+	}
+	
+	public void delete(Protest p) {
+		sesFact.getCurrentSession().delete(p);
+	}
 
 }
--- Ben_Swerdlow_Code/ProtestOrganizer/src/main/java/com/protestorganizer/model/Protest.java
+++ Ben_Swerdlow_Code/ProtestOrganizer/src/main/java/com/protestorganizer/model/Protest.java
@@ -1,5 +1,119 @@
 package com.protestorganizer.model;
 
-public class Protest {
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="protest")
+public class Protest {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="protest_id")
+	private int id;
+	
+	@Column(name="address_id")
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	@JoinTable(name="address")
+	private Address address;
+	
+	@Column(name="start_date_time")
+	private Date startDateTime;
+	
+	@Column(name="estimated_attendance")
+	private int estimatedAttendance;
+	
+	@Column(name="cause")
+	private String cause;
+	
+	public Protest() {
+		super();
+	}
+
+	public Protest(int id, Address address, int estimatedAttendance, String cause) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.estimatedAttendance = estimatedAttendance;
+		this.cause = cause;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Date getStartDateTime() {
+		return startDateTime;
+	}
+
+	public void setStartDateTime(Date startDateTime) {
+		this.startDateTime = startDateTime;
+	}
+
+	public int getEstimatedAttendance() {
+		return estimatedAttendance;
+	}
+
+	public void setEstimatedAttendance(int estimatedAttendance) {
+		this.estimatedAttendance = estimatedAttendance;
+	}
+
+	public String getCause() {
+		return cause;
+	}
+
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+
+	@Override
+	public String toString() {
+		return "Protest [id=" + id + ", addressId=" + address + ", estimatedAttendance=" + estimatedAttendance + ", cause="
+				+ cause + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Protest other = (Protest) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 }
